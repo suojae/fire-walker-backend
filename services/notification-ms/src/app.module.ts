b8c7ff8fdcac4f6362ab03f2c4ecf7d3dbb14dc2
suojae <suojae.developer@gmail.com>
2024-12-15 22:37:35 +0900

    feat: app-module 의존성 주입

--- services/notification-ms/src/app.module.ts
+++ services/notification-ms/src/app.module.ts
@@ -1,7 +1,15 @@
 import { Module } from '@nestjs/common';
+import { HttpModule } from '@nestjs/axios';
+import { ConfigModule } from '@nestjs/config';
 import { NotificationModule } from './presentation/notification.module';
 
 @Module({
-  imports: [NotificationModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    HttpModule,
+    NotificationModule,
+  ],
 })
 export class AppModule {}
