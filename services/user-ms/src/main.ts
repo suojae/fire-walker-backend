f58647d4e9aa4d1ca6372d2c5e2e05cb7f95b19b
suojae <suojae.developer@gmail.com>
2024-12-17 07:53:14 +0900

    feat: main파일 스웨거 초기화 함수 및 config 초기화 함수 추가

--- services/user-ms/src/main.ts
+++ services/user-ms/src/main.ts
@@ -1,9 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { UserModule } from './user.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import * as dotenv from 'dotenv';
 
 async function bootstrap() {
   const app = await NestFactory.create(UserModule);
   console.log('User service is running...');
+  dotenv.config();
+  console.log('DB Password:', process.env.MYSQL_PASSWORD);
+
+  const config = new DocumentBuilder()
+    .setTitle('User-MS API')
+    .setDescription('The API description')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
