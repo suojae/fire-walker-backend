1dbafcbe2b5fdab939efeaa51e13ac943e5fa8e2
suojae <suojae.developer@gmail.com>
2024-12-16 14:34:08 +0900

    feat: auth-dto 추가

---
+++ services/user-ms/src/presentation/dto/auth-request.dto.ts
@@ -0,0 +1,12 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsString } from 'class-validator';
+
+export class AuthRequestDto {
+  @ApiProperty({
+    example: 'abc123authcode',
+    description: '소셜 서버에서 발급받은 AuthCode (카카오/애플 등)',
+  })
+  @IsString()
+  authCode: string;
+}
+
---
+++ services/user-ms/src/presentation/dto/auth-response.dto.ts
@@ -0,0 +1,21 @@
+import { ApiProperty } from '@nestjs/swagger';
+
+export class AuthResponseDto {
+  @ApiProperty({
+    example: '123e4567-e89b-12d3-a456-426614174000',
+    description: '유저 고유 ID(UUID)',
+  })
+  userId: string;
+
+  @ApiProperty({
+    example: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9...',
+    description: 'Access Token (JWT 형식)',
+  })
+  accessToken: string;
+
+  @ApiProperty({
+    example: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ8...',
+    description: 'Refresh Token (JWT 혹은 기타 형식)',
+  })
+  refreshToken: string;
+}
