119502c8738e193a0d0594f5589756efecefc95e
suojae <suojae.developer@gmail.com>
2024-12-17 05:08:32 +0900

    feat: user-module 의존성 주입

--- services/user-ms/src/user.module.ts
+++ services/user-ms/src/user.module.ts
@@ -1,9 +1,30 @@
 import { Module } from '@nestjs/common';
-import { UserController } from './user.controller';
-import { UserService } from './user.service';
+import { ConfigModule } from '@nestjs/config';
+import { UserController } from './presentation/user.controller';
+import { UserUsecase } from './domain/user.usecase';
+import { AuthGateway } from './infrastructure/gateways/auth.gateway';
+import { KakaoAuthService } from './infrastructure/gateways/kakao-auth.service';
+import { KakaoAuthConfig } from './infrastructure/gateways/kakao-auth.config';
+import { NotificationGateway } from './infrastructure/gateways/notification.gateway';
+import { TokenService } from './infrastructure/services/token.service';
 
 @Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+  ],
   controllers: [UserController],
-  providers: [UserService],
+  providers: [
+    UserUsecase,
+    AuthGateway,
+    KakaoAuthService,
+    TokenService,
+    KakaoAuthConfig,
+    NotificationGateway,
+  ],
+  exports: [
+    UserUsecase,
+  ],
 })
 export class UserModule {}
