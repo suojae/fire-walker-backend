f337f7509c0be5a170f07c3ed271fabc8942fb88
suojae <suojae.developer@gmail.com>
2024-12-23 11:10:50 +0900

    chore: dto 속성 수정

--- services/user-ms/src/presentation/dto/friend-request.dto.ts
+++ services/user-ms/src/presentation/dto/friend-request.dto.ts
@@ -1,14 +1,9 @@
 import { ApiProperty } from '@nestjs/swagger';
 
-export class FriendRequestDto {
-  @ApiProperty({ example: 'nickname567', description: '친구 요청을 보낼 닉네임' })
-  friendNickname: string;
-}
+export class FriendRequestNotificationRequestDto {
+  @ApiProperty({ description: '친구 요청을 보낸 유저의 닉네임' })
+  senderNickname: string;
 
-export class FriendAcceptRequestDto {
-  @ApiProperty({
-    example: '123e4567-e89b-12d3-a456-426614174999',
-    description: '수락할 친구 요청의 유저 UUID',
-  })
-  friendUserId: string;
+  @ApiProperty({ description: '친구 요청을 받을 유저의 닉네임' })
+  friendNickname: string;
 }
--- services/user-ms/src/presentation/dto/friend-response.dto.ts
+++ services/user-ms/src/presentation/dto/friend-response.dto.ts
@@ -1,17 +1,6 @@
 import { ApiProperty } from '@nestjs/swagger';
 
-export class FriendResponseDto {
-  @ApiProperty({ example: true, description: '친구 요청 성공 여부' })
-  success: boolean;
-
-  @ApiProperty({ example: 'Request Sent', description: '결과 메시지' })
-  message: string;
-}
-
-export class FriendAcceptResponseDto {
-  @ApiProperty({ example: true, description: '친구 요청 수락 성공 여부' })
-  success: boolean;
-
-  @ApiProperty({ example: 'Friend Accepted', description: '결과 메시지' })
+export class FriendRequestNotificationResponseDto {
+  @ApiProperty({ description: '처리 결과 메시지' })
   message: string;
 }
